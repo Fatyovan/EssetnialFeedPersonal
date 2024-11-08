227dadc2d806a0e63bc600d25c28be33ed3798bf
Ivan Jovanovik <jovanovikj.ivan@gmail.com>
2024-11-08 11:33:01 +0100

    Move RemoteFeedItem to its own file Move LocalFeedItem to its own file

---
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,22 @@
+//
+//  Untitled.swift
+//  EssentialFeed
+//
+//  Created by Ivan Jovanovik on 8.11.24.
+//
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+   public let id: UUID
+   public let description: String?
+   public let location: String?
+   public let imageURL: URL
+    
+    public init(id: UUID, description: String?,
+                location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
