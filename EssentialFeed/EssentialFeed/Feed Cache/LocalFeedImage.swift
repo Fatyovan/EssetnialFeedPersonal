c3e142129eaa4cc5fae7200b5b8a7d86c47cdb14
Ivan Jovanovik <jovanovikj.ivan@gmail.com>
2024-11-08 11:48:31 +0100

    Remove references of "Items" in favor of "Images" which is a domain term used by domain experts in the specs

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedImage.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedImage.swift
@@ -1,22 +1,22 @@
 //
-//  Untitled.swift
+//  LocalFeedImage.swift
 //  EssentialFeed
 //
 //  Created by Ivan Jovanovik on 8.11.24.
 //
 import Foundation
 
-public struct LocalFeedItem: Equatable {
+public struct LocalFeedImage: Equatable {
    public let id: UUID
    public let description: String?
    public let location: String?
-   public let imageURL: URL
+   public let url: URL
     
     public init(id: UUID, description: String?,
-                location: String?, imageURL: URL) {
+                location: String?, url: URL) {
         self.id = id
         self.description = description
         self.location = location
-        self.imageURL = imageURL
+        self.url = url
     }
 }
