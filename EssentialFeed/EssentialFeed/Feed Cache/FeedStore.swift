4e4df2eeb7103969dc9a26c0e9742e77a6ec54d8
Ivan Jovanovik <jovanovikj.ivan@gmail.com>
2024-11-10 20:10:26 +0100

    Load command fails on retrieval error

--- EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -10,8 +10,9 @@ import Foundation
 public protocol FeedStore {
     typealias DeleteionCompletion = (Error?) -> Void
     typealias InsertionCompletion = (Error?) -> Void
+    typealias RetrievalCompletion = (Error?) -> Void
     
     func deleteCachedFeed(completion: @escaping DeleteionCompletion)
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
-    func retrieve()
+    func retrieve(completion: @escaping RetrievalCompletion)
 }
