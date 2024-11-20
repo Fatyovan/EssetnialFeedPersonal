f9876381e035bf272eaf183060fd4ad2dc74383c
Ivan Jovanovik <jovanovikj.ivan@gmail.com>
2024-11-20 10:34:16 +0100

    Make code more readeable

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -81,11 +81,14 @@ extension LocalFeedLoader {
     public func validateCache() {
         store.retrieve { [weak self] result in
             guard let self = self else { return }
+            
             switch result {
             case .failure:
                 self.store.deleteCachedFeed { _ in }
+                
             case let .found(_, timestamp) where !self.validate(timestamp):
                 self.store.deleteCachedFeed { _ in }
+                
             case .empty, .found: break
             }
         }
